c4ca398dbd57d9574d13098936180f6fb68eb290
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-04 02:22:34 +0000

    Automatically backed up by Learn

--- db/migrate/20160114213006_create_posts.rb
+++ db/migrate/20160114213006_create_posts.rb
@@ -7,3 +7,9 @@ class CreatePosts < ActiveRecord::Migration
     end
   end
 end
+
+
+
+#We can set up a many-to-many relationship using a join table. In this case, comments will act as our join table. Any table that contains two foreign keys can be thought of as a join table. A row in our comments table will look something like this:
+# id	content	post_id	user_id
+# 1	"I loved this post!"	5	3
